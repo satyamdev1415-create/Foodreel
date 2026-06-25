04c38b5e7cf19b279830ae3513eb83df5673ba41
satyamdev1415 <satyamdev1415@gmail.com>
2026-06-25 19:54:12 +0530

    Added authentication system

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,logout,authenticate
+from .forms import RegisterForm 
 
 # Create your views here.
+
+def register_view(request):
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+
+    else:
+        form = RegisterForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method ==  "POST":
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user:
+            login(request, user)
+            return redirect('profile')
+
+    return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+
+def profile_view(request):
+    return render(request, 'accounts/profile.html')
