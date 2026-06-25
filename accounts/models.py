7496a15e9e00b5e9ba7436f93ffa5c3972eca848
satyamdev1415 <satyamdev1415@gmail.com>
2026-06-25 18:39:39 +0530

    Added custom user model with roles

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    REEL_CHOICES = (
+        ('customer','Customer'),
+        ('vendor','Vendor'),
+        ('admin', 'Admin'),
+    )
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(max_length=20, choices=REEL_CHOICES, default='customer')
+
+
+
+    def __str__(self):
+        return self.username
+    
