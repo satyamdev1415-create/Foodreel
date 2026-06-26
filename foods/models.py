4be468add8549fd327d2232e32e4154ae3c73bd2
satyamdev1415 <satyamdev1415@gmail.com>
2026-06-26 19:34:15 +0530

    Added Category and FoodItem models

--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from vendors.models import Vendor
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+
+    slug = models.SlugField(unique=True)
+
+    image = models.ImageField(
+        upload_to="categories/",
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+
+class FoodItem(models.Model):
+
+    vendor = models.ForeignKey(
+        Vendor,
+        on_delete=models.CASCADE,
+        related_name='foods'
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2
+    )
+
+    image = models.ImageField(
+        upload_to="foods/"
+    )
+
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
